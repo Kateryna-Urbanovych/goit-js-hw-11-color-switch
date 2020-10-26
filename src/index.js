50d52a6f200220250009d1f892c4ccef26b41135
Kateryna-Urbanovych <katya_urb@ukr.net>
2020-10-26 08:30:24 +0200

    Добавляет color-switch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('qweqweqwe');
+import colors from './js/colors.json';
+import './css/styles.css';
+
+const DELAY = 1000;
+let intervalId = null;
+let isActive = false;
+
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"]'),
+    stopBtn: document.querySelector('button[data-action="stop"]'),
+    body: document.body,
+}
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    if (isActive) {
+        return;
+    }
+    isActive = true;
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+    }, DELAY);
+}
+
+function onStopBtnClick() {
+    clearInterval(intervalId);
+    isActive = false;
+}
+
+function randomIntegerFromInterval (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
